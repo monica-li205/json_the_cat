789c5ed8ef6f02bc432de69f5bcfcb4844759ae5
Monica Li <monicali@live.ca>
2020-11-05 23:45:55 +0000

    fixed code to detect more errors

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,17 @@
 const args = process.argv.slice(2);
 const request = require('request');
 
-request(`https://ap.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
+request(` https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
 
-  if (error === null) {
-    console.log('MREOWER: this is not a breed of cat!!');
-  } else if (error.code === 'ENOTFOUND') {
-    console.log('MREOWER: u have a meowstake in your URL!!');
+  if (JSON.parse(body).length === 0) {
+    console.log('MREOWER: that aint a real cat!!');
+  } else if (error === 'ENOTFOUND') {
+    console.log('MREOWER: u have a (smol) meowstake in your URL!!');
+  } else if (response === undefined || response === 'undefined:1') {
+    console.log('MREOWER: PLWESASE USE A VALID URL!!!');
+  } else if (error === SyntaxError) {
+    console.log('MREOWER: CHECK UR SPELLING!!!');
   } else {
-    console.log('status 200', response.statusCode);
     const data = JSON.parse(body);
     console.log(data[0].description);
   }
